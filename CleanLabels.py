26f844e1628677e35d7f38ce313a4bb502e691ec
Zhen Yao <zhen.yao@keysight.com>
2018-11-29 10:48:50 +0800

    @yz 11/29/2018 add tain and test split function

--- CleanLabels.py
+++ CleanLabels.py
@@ -1,34 +1,51 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-NUM_VALID = 3360
-dataFrm = pd.read_csv('afm.csv')
-data = dataFrm.loc[0:NUM_VALID]
-data = data.drop(['secondaryNoise','fiber'], axis=1)
 
-# print(type(data['noise'].value_counts().plot))
-# data['noise'].value_counts().plot.pie(autopct='%1.1f%%')
-# data['noise'].value_counts().plot.bar()
 
-data.loc[:,'noise'].fillna(value='c', inplace=True)
+def afm_clean_export(src, aim):
+    NUM_VALID = 3360
+    dataFrm = pd.read_csv(src)
+    data = dataFrm.loc[0:NUM_VALID]
+    data = data.drop(['secondaryNoise','fiber'], axis=1)
 
-noiseTypes = []
-for item in ['hs', 'hg', 'hp', 'vg', 'hl', 'vl', 'hb']:
-    data['noise'] = data['noise'].str.replace(item,item[1:])
+    # print(type(data['noise'].value_counts().plot))
+    # data['noise'].value_counts().plot.pie(autopct='%1.1f%%')
+    # data['noise'].value_counts().plot.bar()
 
-cnt = 0;
-for v in data['imPath']:
-    data.loc[cnt,'imName'] = os.path.basename(v)
-    cnt += 1
-    # if(cnt>6):
-    #     break
+    data.loc[:,'noise'].fillna(value='c', inplace=True)
 
-# data.to_csv('afm_clean.csv',index=False)
-plt.subplot(2,1,1)
-data['noise'].value_counts().plot.bar()
-plt.subplot(2,1,2)
-data['noise'].value_counts().plot.pie(autopct='%1.1f%%')
-plt.show()
+    noiseTypes = []
+    for item in ['hs', 'hg', 'hp', 'vg', 'hl', 'vl', 'hb']:
+        data['noise'] = data['noise'].str.replace(item,item[1:])
 
-# {nan,  'hs',  'hg', 'hp',  'vg', 'w', 'hl', 'vl', 'p', 'n', 'x', 'hb'}
+    # cnt = 0;
+    # for v in data['imPath']:
+    #     data.loc[cnt,'imName'] = os.path.basename(v)
+    #     cnt += 1
+
+    data['noise_simple'] = data['noise'].copy()
+    data.loc[data['noise_simple']!='c', 'noise_simple'] = 'n'
+
+    data.to_csv(aim,index=False)
+    # plt.subplot(2,1,1)
+    # data['noise_simple'].value_counts().plot.bar()
+    # plt.subplot(2,1,2)
+    # data['noise_simple'].value_counts().plot.pie(autopct='%1.1f%%')
+    # plt.show()
+
+    # {nan,  'hs',  'hg', 'hp',  'vg', 'w', 'hl', 'vl', 'p', 'n', 'x', 'hb'}
+
+def split(src, train_file, test_file):
+    dfRaw = pd.read_csv(src)
+    dfTrain, dfTest = train_test_split(dfRaw, test_size=0.2, random_state=122)
+    print(dfTrain.head())
+    # dfShuffle = dfRaw.sample(frac=1).reset_index(drop=True)
+    dfTrain[['id','noise_simple']].to_csv(train_file,index=False)
+    dfTest[['id','noise_simple']].to_csv(test_file,index=False)
+
+afm_clean_export('afm.csv','afm_clean.csv')
+
+split('afm_clean.csv','afm_train.csv', 'afm_test.csv')
